89a0888f87fd8aa98aff7e4da2a916414290f0da
albertiriveiro-droid <albertiriveiro@gmail.com>
2026-01-07 17:55:32 +0100

    Despliegue

--- src/api/http.tsx
+++ src/api/http.tsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const http = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL:  "https://bettimeapi-hcaphugjhkabgack.westeurope-01.azurewebsites.net/api",
   headers: { "Content-Type": "application/json" },
 });
 
